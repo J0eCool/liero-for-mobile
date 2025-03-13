fed7c5dcc159a6b9bdf19c04db42f57eb1a4ffa1
J0eCool <count.j0ecool@gmail.com>
2025-03-12 18:25:50 -0700

    basic movement + jumping

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Key thing to figure out: natural/fun control scheme
 # Tech
 
 Engine: [Godot v4.4 Stable, .NET edition](https://godotengine.org/releases/4.4/)  
-Also requires .NET or Mono runtime installed on your dev machine (should be ok to deploy)
+Also requires .NET or Mono runtime installed on your dev machine (should be ok to deploy)  
+Make sure to set the environment variable `GODOT4` to point to the godot executable  
+For more detailed instructions, see [the Godot documentation](https://docs.godotengine.org/en/stable/tutorials/scripting/c_sharp/c_sharp_basics.html)
+
 
 Art assets: [Krita](https://krita.org/en/)
---
+++ Worm.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Worm : CharacterBody2D {
+    [Export] public float Gravity = 500.0f;
+
+    [Export] public float Speeds = 100.0f;
+    [Export] public float JumpHeight = 250.0f;
+
+
+    public override void _Process(double deltaT) {
+        float dt = (float)deltaT;
+
+        Vector2 vel = Velocity;
+        vel.X = 0;
+        if (Input.IsActionPressed("left")) {
+            vel.X -= Speeds;
+        }
+        if (Input.IsActionPressed("right")) {
+            vel.X += Speeds;
+        }
+
+        if (!IsOnFloor()) {
+            vel.Y += Gravity*dt;
+            if (vel.Y*Gravity < 0 && Input.IsActionJustReleased("jump")) {
+                vel.Y *= 1/3.0f;
+            }
+        } else {
+            vel.Y = 0;
+            if (Input.IsActionJustPressed("jump")) {
+                vel.Y = -Mathf.Sign(Gravity)*Mathf.Sqrt(2*Gravity*JumpHeight);
+            }
+        }
+
+        Velocity = vel;
+        MoveAndSlide();
+    }
+}

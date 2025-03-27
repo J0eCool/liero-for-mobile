5ff0ac73bcbf5c4c68c01486a8c8e5633ee43666
J0eCool <count.j0ecool@gmail.com>
2025-03-26 18:37:15 -0700

    Reticle moves up and down

--- Worm.cs
+++ Worm.cs
@@ -5,16 +5,23 @@ using System.Diagnostics;
 public partial class Worm : CharacterBody2D {
 	[Export] public float Gravity = 500.0f;
 
-	[Export] public float Speeds = 100.0f;
+	[Export] public float MoveSpeed = 100.0f;
 	[Export] public float JumpHeight = 250.0f;
-	
+	[Export] public float AimSpeed = 250.0f; // in degrees/s
+
 	// Sprite2D sprite = FindChild("Body") as Sprite2D;
 	private Sprite2D _body;
 	private Sprite2D _gun;
+	private Sprite2D _reticle;
+	
+	private float _aimAngle = 0.0f;
+	private float _reticleDist;
 
 	public override void _Ready() {
 		_body = FindChild("Body") as Sprite2D;
 		_gun = FindChild("Gun") as Sprite2D;
+		_reticle = FindChild("Reticle") as Sprite2D;
+		_reticleDist = _reticle.Position.Length();
 	}
 
 	public override void _Process(double deltaT) {
@@ -28,10 +35,10 @@ public partial class Worm : CharacterBody2D {
 		
 		// Handle H input 
 		if (Input.IsActionPressed("left")) {
-			vel.X -= Speeds;
+			vel.X -= MoveSpeed;
 		}
 		if (Input.IsActionPressed("right")) {
-			vel.X += Speeds;
+			vel.X += MoveSpeed;
 		}
 		
 		// Jump 
@@ -48,16 +55,27 @@ public partial class Worm : CharacterBody2D {
 		}
 
 		// Flip sprites
+		bool isFlipped = _body.FlipH;
 		if (vel.X < 0) {
-			// Face left
-			_body.FlipH = true;
-			_gun.FlipH = true;
+			isFlipped = true;
 		} else if (vel.X > 0) {
-			// Face right
-			_body.FlipH = false;
-			_gun.FlipH = false;
+			isFlipped = false;
+		}
+		_body.FlipH = isFlipped;
+		_gun.FlipH = isFlipped;
+
+		// Handle V input
+		var dAim = Input.GetAxis("up", "down");
+		_aimAngle += dt * dAim * AimSpeed;
+		_aimAngle = Mathf.Clamp(_aimAngle, -85, 45);
+		var rPos = _reticleDist * Vector2.FromAngle(_aimAngle*Mathf.Pi/180);
+		if (isFlipped)
+		{
+			rPos.X *= -1;
 		}
 
+		_reticle.Position = rPos;
+		
 		// Ship it!!!!
 		Velocity = vel;
 		MoveAndSlide();
